2ad71c51afb8588b7deb8625db7ebdd15ded8822
ar-byu <rec21002@byui.edu>
2022-06-02 19:23:35 -0400

    Updated JS, style

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll("[data-src]");
 
-const imageOptions = {}
+const imageOptions = {threshold: 0, rootMargin: "0px 0px 50px 0px"};
 
 function preloadImage(img)  {
   const src = img.getAttribute("data-src");
@@ -10,18 +10,24 @@ function preloadImage(img)  {
 
   img.src = src;
 }
+if('IntersectionObserver' in window) {
+  const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return
+      } else {
+        preloadImage(entry.target);
+        imageObserver.unobserve(entry.target);
+      }
+    })
+  }, imageOptions);
 
-const imageObserver = new IntersectionObserver((entries, imageObserver) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return
-    } else {
-      preloadImage(entry.target);
-      imageObserver.unobserve(entry.target);
-    }
+  images.forEach(image => {
+    imageObserver.observe(image);
   })
-}, imageOptions);
-
-images.forEach(image => {
-  imageObserver.observe(image);
-})
+}
+else {
+  imagesToLoad.forEach((img) => {
+    preloadImage(img);
+  });
+}
