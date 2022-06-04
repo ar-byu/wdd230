ecdcc236be1d76ba44f0a1e28a77a7c542bb7440
ar-byu <rec21002@byui.edu>
2022-06-04 14:02:37 -0400

    Added Discover page, more JS

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -39,3 +39,54 @@ const close = document.querySelector("#close");
 close.addEventListener("click", () => {
     banner.style.display = "none";
 })
+
+// Lazy Loading Script
+
+const images = document.querySelectorAll("[data-src]");
+
+const imageOptions = {threshold: 0, rootMargin: "0px 0px 50px 0px"};
+
+function preloadImage(img)  {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return
+  }
+
+  img.src = src;
+}
+if('IntersectionObserver' in window) {
+  const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return
+      } else {
+        preloadImage(entry.target);
+        imageObserver.unobserve(entry.target);
+      }
+    })
+  }, imageOptions);
+
+  images.forEach(image => {
+    imageObserver.observe(image);
+  })
+}
+else {
+  imagesToLoad.forEach((img) => {
+    preloadImage(img);
+  });
+}
+
+// Local Storage
+
+let lastVisited = window.localStorage.getItem("last-visited-date");
+let currentVisit = new Date();
+window.localStorage.setItem("last-visited-date", currentVisit);
+
+let lastVisitedMessage = "This is your first visit!";
+
+if (lastVisited) {
+    let sinceLastVisit = Math.round((currentVisit - new Date(lastVisited)) / (1000 * 60 * 60 * 24));
+    lastVisitedMessage = "Days since last visit: " + sinceLastVisit;
+}
+
+document.getElementById("last-visited").textContent = lastVisitedMessage;
